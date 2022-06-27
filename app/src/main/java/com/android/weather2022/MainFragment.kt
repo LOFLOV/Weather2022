e53732f9f975708acab9324b18bec0b81ae93f3a
JB <jb>
2022-06-27 10:38:32 +0200

    Added WeatherData.

--- app/src/main/java/com/android/weather2022/MainFragment.kt
+++ app/src/main/java/com/android/weather2022/MainFragment.kt
@@ -5,11 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.android.weather2022.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
 
     lateinit var binding: FragmentMainBinding
+    lateinit var viewModel: MainViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,5 +19,10 @@ class MainFragment : Fragment() {
     ): View {
         binding = FragmentMainBinding.inflate(inflater)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
     }
 }
---
+++ app/src/main/java/com/android/weather2022/MainViewModel.kt
@@ -0,0 +1,8 @@
+package com.android.weather2022
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel(val liveData: MutableLiveData<Any> = MutableLiveData<Any>()): ViewModel() {
+
+}
