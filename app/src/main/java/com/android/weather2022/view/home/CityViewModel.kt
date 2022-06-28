f93cf2fa6b43e7c285b07714f20ed0dc16fd9303
JB <jb>
2022-06-28 22:00:59 +0200

    Created City ViewModel.

---
+++ app/src/main/java/com/android/weather2022/view/home/CityViewModel.kt
@@ -0,0 +1,21 @@
+package com.android.weather2022.view.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.android.weather2022.network.entity.Weather
+import com.android.weather2022.network.repository.WeatherRepository
+import com.android.weather2022.network.repository.WeatherRepositoryImpl
+
+class CityViewModel: ViewModel() {
+
+    private val weatherRepository: WeatherRepository = WeatherRepositoryImpl()
+
+    private val _cityListLiveData: MutableLiveData<List<Weather>> = MutableLiveData<List<Weather>>()
+    val cityListLiveData: LiveData<List<Weather>> get() = _cityListLiveData
+
+    fun getCityList() {
+        val cityList = weatherRepository.getCityList()
+        _cityListLiveData.postValue(cityList)
+    }
+}
