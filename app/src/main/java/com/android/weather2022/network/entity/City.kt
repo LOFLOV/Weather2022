629da3bf4c62551a5885b4e009e37ac5f7f3c748
JB <jb>
2022-07-01 18:48:29 +0200

    Added Parcelable implementation.

--- app/src/main/java/com/android/weather2022/network/entity/City.kt
+++ app/src/main/java/com/android/weather2022/network/entity/City.kt
@@ -1,7 +1,11 @@
 package com.android.weather2022.network.entity
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class City(
     val name: String,
     val lat: Double,
     val lon: Double
-)
+): Parcelable
--- app/src/main/java/com/android/weather2022/network/entity/Weather.kt
+++ app/src/main/java/com/android/weather2022/network/entity/Weather.kt
@@ -1,7 +1,11 @@
 package com.android.weather2022.network.entity
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Weather(
     val city: City = City("Москва", 55.755826, 37.617299900000035),
     val temperature: Int = 20,
     val feelsLike: Int = 20
-)
+): Parcelable
